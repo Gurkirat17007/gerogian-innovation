636e87fb244bebb779fd36a5a464c636578e6259
Gurkirat17007 <41137077+Gurkirat17007@users.noreply.github.com>
2020-03-23 04:30:23 -0400

    Add files via upload

--- authenticateadmin.php
+++ authenticateadmin.php
@@ -19,49 +19,9 @@ if ( mysqli_connect_errno() ) {
 //	die ('Please fill both the username and password field!');
 //}
 
-
-//ACCOUNTS TABLE
-
+//ADMIN ACCOUNTS TABLE
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-    
-    if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-	// Account exists, now we verify the password.
-	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-             
-	if (password_verify($_POST['password'], $password)) {
-		// Verification success! User has loggedin!
-		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
-		session_regenerate_id();
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
-		header('Location: home.php');
-	} else {
-		echo 'Incorrect password!';
-	}
-} else {
-	echo 'Incorrect username!';
-}
-
-	$stmt->close();
-}
-
-
-
-
-
-
-//FACULTY STAFF ACCOUNTS TABLE
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM facultystaffaccounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT id, password FROM adminaccounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -80,7 +40,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM facultystaffaccounts WHERE u
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
 		$_SESSION['id'] = $id;
-		header('Location: facultystaffhome.php');
+		header('Location: adminhome.php');
 	} else {
 		echo 'Incorrect password!';
 	}
@@ -90,6 +50,4 @@ if ($stmt = $con->prepare('SELECT id, password FROM facultystaffaccounts WHERE u
 
 	$stmt->close();
 }
-
-
 ?>
